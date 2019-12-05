57af9f0b5ca58e8cda152250429fe204f0137513
Alex Dutton <alex@cottagelabs.com>
2019-12-05 15:12:15 +0000

    Allow JSON-LD serializer mimetype to be overridden

--- invenio_sword/serializers.py
+++ invenio_sword/serializers.py
@@ -5,10 +5,11 @@ from flask import Response, request
 sword_jsonld_context = "https://swordapp.github.io/swordv3/swordv3.jsonld"
 
 
-def jsonld_serializer(data):
+def jsonld_serializer(data, **kwargs):
+    kwargs.setdefault("mimetype", "application/ld+json")
     data = {
         "@context": sword_jsonld_context,
         "@id": data.get("@id") or request.url,
         **data,
     }
-    return Response(json.dumps(data, indent=2) + "\n", mimetype="application/ld+json")
+    return Response(json.dumps(data, indent=2) + "\n", **kwargs)
