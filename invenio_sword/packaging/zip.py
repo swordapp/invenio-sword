a47ae7afa18dd9bc922b9e65f02702f0ee1e3552
Alex Dutton <alex@cottagelabs.com>
2019-12-05 15:02:47 +0000

    Add initial simple ZIP packaging

--- invenio_sword/packaging/zip.py
+++ invenio_sword/packaging/zip.py
@@ -1,7 +1,35 @@
+import mimetypes
+import shutil
+import tempfile
+import typing
+import zipfile
+
+from invenio_files_rest.models import ObjectVersion
+from invenio_sword.api import SWORDDeposit
 from .base import Packaging
 
 __all__ = ["SimpleZipPackaging"]
 
 
 class SimpleZipPackaging(Packaging):
-    pass
+    def ingest(
+        self,
+        *,
+        record: SWORDDeposit,
+        stream: typing.BinaryIO,
+        filename: str = None,
+        content_type: str
+    ):
+        with tempfile.TemporaryFile() as f:
+            shutil.copyfileobj(stream, f)
+            f.seek(0)
+
+            zip = zipfile.ZipFile(f)
+
+            for name in zip.namelist():
+                ObjectVersion.create(
+                    record.bucket,
+                    name,
+                    mimetype=mimetypes.guess_type(name)[0],
+                    stream=zip.open(name),
+                )
