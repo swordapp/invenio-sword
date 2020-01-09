484126de165e2557dd13d4b784e3212af0f7c98d
Alex Dutton <alex@cottagelabs.com>
2020-01-09 16:15:15 +0000

    Use typing-extensions for compatibility with Python versions <3.8.
    We are using typing.Protocol for static and runtime type-checking, which is Py
    3.8 onwards.

--- invenio_sword/typing.py
+++ invenio_sword/typing.py
@@ -1,8 +1,12 @@
-from typing import Protocol
-from typing import runtime_checkable
+try:
+    from typing import Protocol
+    from typing import runtime_checkable
+except ImportError:  # pragma: nocover
+    from typing_extensions import Protocol  # type: ignore
+    from typing_extensions import runtime_checkable
 
 
 @runtime_checkable
 class BytesReader(Protocol):
     def read(self, amount: int = -1) -> bytes:
-        ...
+        ...  # pragma: nocover
--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ install_requires = [
     "invenio-records-ui>=1.0.1",
     "invenio-search-ui>=1.0.0a5",
     "rfc6266-parser",
+    # We use typing.Protocol, which is Py3.8+, but is available in typing-extensions for backwards compatibility
+    'typing-extensions;python_version<"3.8"',
 ]
 
 packages = find_packages()
