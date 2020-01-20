a0b61b81c8b1de22d17066511d6f0bda0209550d
Alex Dutton <alex@cottagelabs.com>
2020-01-20 14:48:01 +0000

    Test ingesting in status endpoint, alongside service document endpoint

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,3 +1,5 @@
+import io
+import json
 import os
 from http import HTTPStatus
 
@@ -29,57 +31,58 @@ packaged_content = {
     },
 }
 
+ingest_test_parameters = [
+    # A binary deposit
+    (
+        "binary.svg",
+        "http://purl.org/net/sword/3.0/package/Binary",
+        "image/svg+xml",
+        {
+            "binary.svg": {
+                "rel": [
+                    "http://purl.org/net/sword/3.0/terms/fileSetFile",
+                    "http://purl.org/net/sword/3.0/terms/originalDeposit",
+                ],
+                "contentType": "image/svg+xml",
+                "status": "http://purl.org/net/sword/3.0/filestate/ingested",
+            }
+        },
+    ),
+    # A simple zip deposit
+    (
+        "simple.zip",
+        "http://purl.org/net/sword/3.0/package/SimpleZip",
+        "application/zip",
+        {
+            original_deposit: {
+                "rel": ["http://purl.org/net/sword/3.0/terms/originalDeposit"],
+                "contentType": "application/zip",
+                "status": "http://purl.org/net/sword/3.0/filestate/ingested",
+                "packaging": "http://purl.org/net/sword/3.0/package/SimpleZip",
+            },
+            **packaged_content,  # type: ignore
+        },
+    ),
+    # A BagIt deposit
+    (
+        "bagit.zip",
+        "http://purl.org/net/sword/3.0/package/SWORDBagIt",
+        "application/zip",
+        {
+            original_deposit: {
+                "rel": ["http://purl.org/net/sword/3.0/terms/originalDeposit"],
+                "contentType": "application/zip",
+                "status": "http://purl.org/net/sword/3.0/filestate/ingested",
+                "packaging": "http://purl.org/net/sword/3.0/package/SWORDBagIt",
+            },
+            **packaged_content,  # type: ignore
+        },
+    ),
+]
+
 
 @pytest.mark.parametrize(
-    "filename,packaging,content_type,expected_links",
-    [
-        # A binary deposit
-        (
-            "binary.svg",
-            "http://purl.org/net/sword/3.0/package/Binary",
-            "image/svg+xml",
-            {
-                "binary.svg": {
-                    "rel": [
-                        "http://purl.org/net/sword/3.0/terms/fileSetFile",
-                        "http://purl.org/net/sword/3.0/terms/originalDeposit",
-                    ],
-                    "contentType": "image/svg+xml",
-                    "status": "http://purl.org/net/sword/3.0/filestate/ingested",
-                }
-            },
-        ),
-        # A simple zip deposit
-        (
-            "simple.zip",
-            "http://purl.org/net/sword/3.0/package/SimpleZip",
-            "application/zip",
-            {
-                original_deposit: {
-                    "rel": ["http://purl.org/net/sword/3.0/terms/originalDeposit"],
-                    "contentType": "application/zip",
-                    "status": "http://purl.org/net/sword/3.0/filestate/ingested",
-                    "packaging": "http://purl.org/net/sword/3.0/package/SimpleZip",
-                },
-                **packaged_content,  # type: ignore
-            },
-        ),
-        # A BagIt deposit
-        (
-            "bagit.zip",
-            "http://purl.org/net/sword/3.0/package/SWORDBagIt",
-            "application/zip",
-            {
-                original_deposit: {
-                    "rel": ["http://purl.org/net/sword/3.0/terms/originalDeposit"],
-                    "contentType": "application/zip",
-                    "status": "http://purl.org/net/sword/3.0/filestate/ingested",
-                    "packaging": "http://purl.org/net/sword/3.0/package/SWORDBagIt",
-                },
-                **packaged_content,  # type: ignore
-            },
-        ),
-    ],
+    "filename,packaging,content_type,expected_links", ingest_test_parameters
 )
 def test_ingest(
     api,
@@ -109,6 +112,71 @@ def test_ingest(
                 },
             )
         assert response.status_code == HTTPStatus.CREATED
+
+        response = client.get(response.headers["Location"])
+
+        for link in response.json["links"]:
+            print(link)
+            key = link["@id"].split("/", 7)[-1]
+            if (
+                "http://purl.org/net/sword/3.0/terms/originalDeposit" in link["rel"]
+                and "http://purl.org/net/sword/3.0/terms/fileSetFile" not in link["rel"]
+            ):
+                expected_link = expected_links[original_deposit]
+            else:
+                expected_link = expected_links[key]
+
+            if "derivedFrom" in link:
+                link["derivedFrom"] = original_deposit
+
+            expected_link["@id"] = response.json["@id"] + "/file/" + key
+
+            assert expected_link == link
+
+        assert len(response.json["links"]) == len(expected_links)
+
+
+@pytest.mark.parametrize(
+    "filename,packaging,content_type,expected_links", ingest_test_parameters
+)
+def test_create_with_metadata_and_then_ingest(
+    api,
+    users,
+    location,
+    es,
+    fixtures_path,
+    filename,
+    packaging,
+    content_type,
+    expected_links,
+):
+    with api.test_request_context(), api.test_client() as client:
+        client.post(
+            url_for_security("login"),
+            data={"email": users[0]["email"], "password": "tester"},
+        )
+
+        response = client.post(
+            "/sword/service-document",
+            data=io.BytesIO(json.dumps({}).encode()),
+            headers={
+                "Metadata-Format": "http://purl.org/net/sword/3.0/types/Metadata",
+                "Content-Type": "application/ld+json",
+                "Content-Disposition": "attachment; metadata=true",
+            },
+        )
+        assert response.status_code == HTTPStatus.CREATED
+
+        with open(os.path.join(fixtures_path, filename), "rb") as f:
+            client.post(
+                response.headers["Location"],
+                data=f,
+                headers={
+                    "Packaging": packaging,
+                    "Content-Type": content_type,
+                    "Content-Disposition": "attachment; filename={}".format(filename),
+                },
+            )
 
         response = client.get(response.headers["Location"])
 
