f4407934c9a84a871ced5a58c84d0511b4643e1a
Alex Dutton <alex@cottagelabs.com>
2020-01-27 12:05:20 +0000

    Sphinx config additions
    These make it look a bit more like the config in invenio-deposit

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+from typing import Any
+from typing import Dict
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -13,10 +17,15 @@
 # sys.path.insert(0, os.path.abspath('.'))
 """Sphinx configuration."""
 # -- Project information -----------------------------------------------------
-
 project = "Invenio-SWORD"
 copyright = "2019, CottageLabs <hello@cottagelabs.com>"
 author = "CottageLabs <hello@cottagelabs.com>"
+
+# Get the version string. Cannot be done with import!
+g: Dict[str, Any] = {}
+with open(os.path.join("..", "invenio_sword", "version.py"), "rt") as fp:
+    exec(fp.read(), g)
+    release = g["__version__"]
 
 
 # -- General configuration ---------------------------------------------------
@@ -25,7 +34,11 @@ author = "CottageLabs <hello@cottagelabs.com>"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
     "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -43,6 +56,21 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "alabaster"
+
+html_theme_options = {
+    "description": "Module for depositing record metadata and uploading files using the SWORD protocol.",
+    "github_user": "swordapp",
+    "github_repo": "invenio-sword",
+    "github_button": False,
+    "github_banner": True,
+    "show_powered_by": False,
+    "extra_nav_links": {
+        "invenio-sword@GitHub": "https://github.com/swordapp/invenio-sword",
+        # Not yet uploaded to PyPI
+        # 'invenio-sword@PyPI': 'https://pypi.python.org/pypi/invenio-sword/',
+        "SWORDv3 specification": "https://swordapp.github.io/swordv3/swordv3.html",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
