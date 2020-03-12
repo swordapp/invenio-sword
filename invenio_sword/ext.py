1b7b6cc7e247a4d1aa741042a5bbb3c858248f2d
Alex Dutton <alex@cottagelabs.com>
2020-03-12 15:25:54 +0000

    Remove unnecessary/unused/unspecified /.well-known implementation

--- invenio_sword/ext.py
+++ invenio_sword/ext.py
@@ -16,12 +16,3 @@ class InvenioSword(object):
 
     def init_app(self, app):
         app.register_blueprint(views.create_blueprint(app.config["SWORD_ENDPOINTS"]))
-
-
-class InvenioSwordWellKnown(object):
-    def __init__(self, app=None):
-        if app:
-            self.init_app(app)
-
-    def init_app(self, app):
-        app.register_blueprint(views.create_wellknown_blueprint())
